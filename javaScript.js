53142260bdae29398bab2262b9236c5b574ec5d5
kamalrajms <kamalrajindtn@gmail.com>
2025-08-05 16:00:52 +0530

    vareables

---
+++ javaScript.js
@@ -0,0 +1,57 @@
+/*console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+console.log("Hello"); */
+
+// console.log();
+
+// vareable
+var person1 = "chandru";
+var person1 = "Kamal";
+console.log(person1);
+
+let person2 = "chandru B";
+person2 = "Praveen";
+console.log(person2);
+
+const person3 = "Keerthi";
+
+console.log(person3);
+
+// data types
+
+let name = "Hello World";
+console.log(name);
+console.log(typeof name);
+
+let mark = 95;
+let price = 99.5;
+console.log(mark);
+console.log(typeof mark);
+console.log(typeof price);
+console.log(price);
+
+let isStudent = true;
+console.log(typeof isStudent);
+console.log(isStudent);
+
+let x;
+console.log(typeof x);
+console.log(x);
+
+let y = null;
+console.log(typeof y);
+console.log(y);
+
+// operators
+
+// arithematic
+let a = 10;
+let b = 5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
