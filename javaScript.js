13eff507bfd0de7529a351732cdaf322082a0bff
kamalrajms <kamalrajindtn@gmail.com>
2025-08-11 16:08:43 +0530

    looping concepts

--- javaScript.js
+++ javaScript.js
@@ -1,16 +1,46 @@
-let age = prompt("Enter your age:");
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
 
-if (age < 18) {
-  console.log("can't Drive");
-  if (age > 5 && age < 12) {
-    console.log("Childerns");
-  } else if (age >= 1 && age < 5) {
-    console.log("Baby");
-  } else {
-    console.log("Hello world");
+let sum = 0;
+for (let a = 1; a <= 5; a++) {
+  // for(){
+
+  // }
+  sum += a;
+  // sum=sum+a
+  console.log("sum", sum);
+}
+
+let b = 1;
+while (b <= 10) {
+  // while(){
+
+  // }
+  console.log(b);
+  b++;
+}
+
+let c = 1;
+do {
+  console.log(c);
+  c++;
+} while (c <= 0);
+
+// Break
+
+for (let i = 1; i <= 10; i++) {
+  if (i === 5) {
+    break;
   }
-} else if (age >= 18 && age <= 80) {
-  console.log("can Drive");
-} else {
-  console.log("must can't drive");
+  console.log(i);
+}
+
+//continue
+
+for (let i = 1; i <= 5; i++) {
+  if (i == 3) {
+    continue;
+  }
+  console.log(i);
 }
