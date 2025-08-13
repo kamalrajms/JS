919d761c5e2c08b17ce7a4996c225b6cd757ed24
kamalrajms <kamalrajindtn@gmail.com>
2025-08-13 15:34:24 +0530

    Array

--- javaScript.js
+++ javaScript.js
@@ -1,14 +1,56 @@
-function isPrime(num) {
-  if (num <= 1) {
-    return false;
-  }
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) {
-      return false;
-    }
-  }
-  return true;
+let fruits = ["apple", "banana", "Mango"];
+let arr = new Array("a", "b", "c", 4, 5, 67, 89);
+console.log(fruits);
+console.log(fruits[0]);
+console.log(fruits[2]);
+console.log(arr);
+console.log(arr.length);
+console.log(arr.length - 1);
+
+let animal = ["Dog", "Cat", "Rabbit"];
+for (let i = 0; i < animal.length; i++) {
+  console.log(animal[i]);
 }
-console.log(isPrime(7));
-console.log(isPrime(10));
-console.log(isPrime(1));
+
+// for of
+
+for (let n of animal) {
+  console.log(n);
+}
+
+// for Each
+animal.forEach(function (i) {
+  console.log(i);
+});
+
+//for =>map,reduce,find,filter
+
+let person = ["chandru m", "chandru v", "keerthi"];
+person.push("kamal");
+console.log(person);
+
+let newPerson = person.pop();
+console.log(newPerson);
+console.log(person);
+
+console.log(person.shift());
+console.log(person.unshift("vijay"));
+console.log(person);
+
+let number = ["Apple", "Banana", "Orange", "Grapes", "Vijay"];
+let x = number.slice(1, 3);
+console.log(x);
+console.log(number);
+
+// splice(start,deletecount,"","")    splice(2,1,"sk")
+
+number.splice(1, 0, "Kiwi", "Mango");
+console.log(number);
+
+console.log(number.indexOf("vijay"));
+
+console.log(number.includes("vijay"));
+
+console.log(number.reverse());
+
+console.log(number.sort());
