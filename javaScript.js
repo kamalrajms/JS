39994ed344d74913988cd3fe96451b3f4899d902
kamalrajms <kamalrajindtn@gmail.com>
2025-08-18 15:49:01 +0530

    selectors in dom

--- javaScript.js
+++ javaScript.js
@@ -1,8 +1,31 @@
 document.getElementById("hello").textContent = "hello Js";
 
+// ID
 let heading = document.getElementById("java");
 
 heading.style.color = "red";
 
-let nam = document.getElementsByClassName("name");
-nam.style.color = "red";
+//class
+let name = document.getElementsByClassName("name");
+
+for (let e of name) {
+  e.style.color = "yellow";
+}
+
+//tag
+
+const tag = document.getElementsByTagName("h1");
+
+console.log(tag.length);
+
+//query selector
+
+const query = document.querySelector(".text");
+query.style.backgroundColor = "red";
+
+//queryselectorall
+
+const allelements = document.querySelectorAll(".textcontent");
+allelements.forEach((e) => {
+  e.style.color = "blue";
+});
