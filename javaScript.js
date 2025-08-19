9679b9afa653f3794145baa8b63debb46d0a6632
kamalrajms <kamalrajindtn@gmail.com>
2025-08-19 16:03:30 +0530

    multi line styleing and single

--- javaScript.js
+++ javaScript.js
@@ -14,4 +14,12 @@ text.classList.add("bold");
 text.classList.remove("java");
 console.log(text.classList.contains("bold"));
 
+//multi line styleing
+let msg = document.getElementById("msg");
+msg.style.color = "red";
+msg.style.fontSize = "20px";
 
+//single linne
+
+let day = document.getElementById("day");
+day.style.cssText = "color:green;font-size:25px;padding:50px";
