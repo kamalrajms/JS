bd34b6c9f7d1e8fdc2f1aa92c915c6a455b827be
kamalrajms <kamalrajindtn@gmail.com>
2025-08-20 16:07:35 +0530

    calculator

--- javaScript.js
+++ javaScript.js
@@ -1,11 +1,31 @@
-//Dunamically updating form fields
-const number = document.getElementById("number");
-const otherfield = document.getElementById("otherfield");
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll(".btn");
+const clear = document.getElementById("clear");
+const equal = document.getElementById("equal");
 
-number.addEventListener("change", () => {
-  if (number.value === "other") {
-    otherfield.style.display = "block";
-  } else {
-    otherfield.style.display = "none";
+//hand btns click
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const value = e.target.value;
+    if (value !== undefined) {
+      display.value += value;
+    }
+  });
+});
+
+//hand clear input
+
+clear.addEventListener("click", () => {
+  display.value = "";
+});
+
+//handle equal(=)
+
+equal.addEventListener("click", () => {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
   }
 });
